8bb0553cdb1207d3580dd448aab8ff5fef670016
Smes21540 <Kevin.goessens@smes-france.com>
2026-02-09 12:48:40 +0100

    Update drive.js

--- netlify/functions/drive.js
+++ netlify/functions/drive.js
@@ -203,10 +203,11 @@ if (list) {
     const params = new URLSearchParams();
     // ⚠️ ne pas encodeURIComponent(id) à l'intérieur de q
     params.set("q", `'${id}' in parents and trashed=false`);
-    params.set(
+params.set(
   "fields",
-  "nextPageToken, files(id,name,mimeType,size,createdTime,modifiedTime,shortcutDetails)"
+  "nextPageToken, files(id,name,mimeType,size,createdTime,modifiedTime,shortcutDetails(targetId,targetMimeType))"
 );
+
 
     params.set("pageSize", "1000");
     params.set("supportsAllDrives", "true");
